7cddb5d2e35ad67e73da4aa9a3fbf0e200c19bb9
PierreBaudoin <pierre.rellic@gmail.com>
2022-04-02 20:25:48 +0200

    idem (need to pull)

--- Assets/Scipt/Rooms/Workingroom.cs
+++ Assets/Scipt/Rooms/Workingroom.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class Workingroom : Room
 {
-    public override void UsedRoom(Character character)
+    public override void StartUseRoom(Character character)
     {
         
+    }
+
+    public override void UseRoomUpdate(Character character)
+    {
+        
+    }
+
+    public override void EndUseRoom(Character character)
+    {
+        
+    }
+
+    private float GetProductivity(Character character)
+    {
+        float productivity;
+
+        // productivity = charactersInRoom.
+
+        return productivity;
     }
 }
