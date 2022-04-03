9acf8da2c7216da276318f9b1045cbd3a69457ef
Skaje <sorignet.gautrot.arthur@gmail.com>
2022-04-03 16:14:32 +0200

    chara + tweek bullet

--- Assets/Scipt/ScoreBullet.cs
+++ Assets/Scipt/ScoreBullet.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreBullet : MonoBehaviour
 {
     public float velocity;
     private RectTransform rect, target;
     Workingroom room;
+    private Image image;
+    private float timer;
+    private float phase1 = 1.5f;
+    private float phase2 = 3f;
 
     void Awake()
     {
@@ -17,10 +22,38 @@ public class ScoreBullet : MonoBehaviour
     {
         this.room = room;
         rect.position =  Camera.main.WorldToScreenPoint(position);
+        timer = 0;
     }
 
     void Update()
     {
+        timer += Time.deltaTime;
+        if (timer < phase1)
+        {
+            GoUp();
+        }
+        else if (timer < phase2){}
+        else {
+            GoToScoreBar();
+            }
+
+        }
+
+    
+    void GoUp (){
+        if(Mathf.Abs(rect.position.x - target.position.x + rect.position.y - target.position.y) <= 5f)
+        {
+            GameManager.instance.Score(5);
+            room.PoolInBullet(this);
+        }
+        else
+        {
+            this.transform.position += transform.up.normalized * velocity/2 * Time.deltaTime;
+        }
+    }
+
+
+    void GoToScoreBar() {
         if(Mathf.Abs(rect.position.x - target.position.x + rect.position.y - target.position.y) <= 5f)
         {
             GameManager.instance.Score(5);
